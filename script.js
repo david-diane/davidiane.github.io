f764af84aabe19e1dedee1fc1804fdf6a33139fa
david-diane <daviddianekouadio@gmail.com>
2026-05-21 15:44:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function envoyerMessage() {
-    let nom = document.getElementById("nom").value;
-    let email = document.getElementById("email").value;
-    let message = document.getElementById("message").value;
+document.getElementById("contact-form").addEventListener("submit", function(e) {
 
-    if(nom === "" || email === "" || message === "") {
-        alert("Veuillez remplir tout les champs !");
-    }   else {
-        alert("Message envoyé avec succés ! Je vous répondrai bientôt.");
-        document.getElementById("contact-form").reset();
-    }
-}
+  let nom = document.getElementById("nom").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  if (nom === "" || email === "" || message === "") {
+    e.preventDefault();
+    alert("Veuillez remplir tous les champs !");
+  }
+
+});
